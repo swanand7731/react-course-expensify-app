282672a0e513debc2fbe13c34b7d7598dd510281
Swan <28915682+swanand7731@users.noreply.github.com>
2020-08-23 00:51:12 +0530

    Setup production webpack build

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ExpenseForm from "./ExpenseForm";
-import { connect } from "react-redux";
-import { addExpense } from "../actions/expenses";
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
     this.props.addExpense(expense);
     this.props.history.push('/');
-  }
-  render(){
+  };
+  render() {
     return (
       <div>
         <h1>Add Expense</h1>
@@ -16,7 +16,7 @@ export class AddExpensePage extends React.Component {
           onSubmit={this.onSubmit}
         />
       </div>
-    )
+    );
   }
 }
 
@@ -24,4 +24,4 @@ const mapDispatchToProps = (dispatch) => ({
   addExpense: (expense) => dispatch(addExpense(expense))
 });
 
-export default connect()(AddExpensePage);
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
